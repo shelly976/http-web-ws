313cf3beba6f0ecfba913e68e8a7c13bdea5e373
Shelly <git remote add origin https://github.com/lakhwinder132/automatic-meme>
2026-05-07 16:55:32 +0530

    updated code

--- packages/mongodb/database.ts
+++ packages/mongodb/database.ts
@@ -2,7 +2,7 @@ import mongoose, { model } from 'mongoose';
 
 async function connector(){
     console.log('connecting to mongodb...');
-    await mongoose.connect('mongodb://localhost:27017/yes');
+    await mongoose.connect('mongodb+srv://shelly:zFNYxBFtjSKw75ib@cluster0.dopcir3.mongodb.net/?appName=Cluster0/yes');
     console.log('connected');
 }
 connector();
